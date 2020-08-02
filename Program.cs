4570776b55ec5c14dfaa27ddb1f172a3c421e2c6
auo <1984096+Auo@users.noreply.github.com>
2020-08-02 19:30:57 +0200

    Initial commit
    - description on how to build and use

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.InteropServices;
+using System;
+
+namespace swap
+{
+    class SwapMouse
+    {
+        [DllImport("user32.dll")]
+        public static extern Int32 SwapMouseButton(Int32 bSwap);
+        static void Main(string[] args)
+        {
+            int rightButtonIsAlreadyPrimary = SwapMouseButton(1);
+
+            if (rightButtonIsAlreadyPrimary != 0) {
+                SwapMouseButton(0);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# swap
+Easy invert mouse configuration, easily. Perfect when you for some reason has two mice connected
+to the same computer..🖱 🖱
+
+1. Build with `dotnet build`
+2. Create shortcut to the `.dll` that has been build.
+	`"C:\Program Files\dotnet\dotnet.exe" C:\path\to\swap.dll`
+3. Enjoy easy switching mouse configuration with `WIN` + name of shortcut
+
